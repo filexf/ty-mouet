dcac9f5406f3e758df5360a175a65d8322b26775
LÃeo TREMOUREUX <ltremoureux@hotmail.fr>
2024-11-18 16:36:57 +0100

    creation users with devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -72,6 +72,7 @@ Rails.application.configure do
   # config.active_job.queue_name_prefix = "ty_mouet_production"
 
   config.action_mailer.perform_caching = false
+  config.action_mailer.default_url_options = { host: 'ty-mouet-c5897a5b74b0' }
 
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
