fe5adcc8d2ade0a316787867b6ba0c32fd14529a
LÃeo TREMOUREUX <ltremoureux@hotmail.fr>
2024-11-18 18:29:07 +0100

    create model of booking

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_152216) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_18_172725) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bookings", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
+    t.string "status"
+    t.bigint "mouette_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["mouette_id"], name: "index_bookings_on_mouette_id"
+  end
 
   create_table "mouettes", force: :cascade do |t|
     t.string "name"
@@ -26,11 +35,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_152216) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-   end
-
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_152826) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "last_name"
@@ -45,7 +50,6 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_152826) do
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-  
+
+  add_foreign_key "bookings", "mouettes"
 end
-
-
