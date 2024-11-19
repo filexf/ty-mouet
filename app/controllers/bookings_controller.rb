6a70d080d98b0c980eea69896e7143db391e67ba
Caroline Cerbelaud <caroline.cerbelaud@gmail.com>
2024-11-19 15:49:58 +0100

    Fix small issues on create booking method

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -5,15 +5,30 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @mouette = Mouette.find(params[:id])
+    @mouette = Mouette.find(params[:mouette_id])
     @booking = Booking.new(booking_params)
     @booking.renter = current_user
     @booking.mouette = @mouette
+    if @booking.save
+      redirect_to booking_path(@booking)
+    else
+      # doit rendre le formulaire à nouveau mais complété
+      # donc la show de mouette : comment faire ?
+      render "mouettes/show", status: :unprocessable_entity
+    end
+  end
+
+  def owner_bookings
+    @bookings = Booking.where(mouette: {owner: current_user})
+  end
+
+  def renter_bookings
+    @bookings = Booking.where(renter: current_user)
   end
 
   private
 
   def booking_params
-    params.require(:booking).permit(:start_date, :end_date, :mouette_id)
+    params.require(:booking).permit(:start_date, :end_date)
   end
 end
