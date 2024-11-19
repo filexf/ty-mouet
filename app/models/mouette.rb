0e16fa3acc3a2a9b8e2163961ff2e856dc4dbb61
Marion Vives <mvives.dev@gmail.com>
2024-11-19 11:49:54 +0100

    modif modele mouettes + foreign key users

--- app/models/mouette.rb
+++ app/models/mouette.rb
@@ -1,4 +1,6 @@
 class Mouette < ApplicationRecord
   validates :name, :availability, presence: true
   validates :description, length: { minimum: 10 }
+  belongs_to :owner, class_name: "User"
+  has_many :bookings
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_11_18_172725) do
+ActiveRecord::Schema[7.1].define(version: 2024_11_19_102620) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -35,6 +35,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_172725) do
     t.float "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "owner_id"
+    t.index ["owner_id"], name: "index_mouettes_on_owner_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -52,4 +54,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_11_18_172725) do
   end
 
   add_foreign_key "bookings", "mouettes"
+  add_foreign_key "mouettes", "users", column: "owner_id"
 end
