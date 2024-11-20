457448e069b347c7a16db164b531686fa7ed4478
felix orain <felix.orain@gmail.com>
2024-11-20 12:41:03 +0100

    Validations model mouette finies

--- app/models/mouette.rb
+++ app/models/mouette.rb
@@ -1,7 +1,30 @@
 class Mouette < ApplicationRecord
-  validates :name, :availability, presence: true
-  validates :description, length: { minimum: 10 }
+
   belongs_to :owner, class_name: "User"
   has_many :bookings
   has_one_attached :photo
+
+  ACCESSORIES = ["casque", "slip", "chaussures"]
+  AVAILABILITY = ["✅ Available ", "❌ Not available "]
+
+
+  validates :name, :availability, :price, :photo, :description, presence: true
+  # validates :name, :uniqueness: true { message: "Name already taken" }
+
+
+  validates :availability, inclusion: {
+      in: AVAILABILITY,
+      message: "Must select : #{AVAILABILITY}"
+  }
+
+  validates :accessories, inclusion: {
+      in: ACCESSORIES,
+      message: "Must select : #{ACCESSORIES}"
+  }
+
+  validates :price, numericality: { greater_than: 0, message: "must be a number greater than 0" }
+
+  validates :description, length: { minimum: 10 }
+  validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0,less_than_or_equal_to: 5 }
+
 end
