9d4b5d374a52ec9c95b2360643772051abab2f75
Marion Vives <mvives.dev@gmail.com>
2024-11-22 14:39:40 +0100

    changement des paramètres de recherche

--- app/models/mouette.rb
+++ app/models/mouette.rb
@@ -9,11 +9,29 @@ class Mouette < ApplicationRecord
   has_many :bookings
   has_one_attached :photo
 
-  pg_search_scope :search_by_name_and_category,
-    against: [ :name, :category ],
-    using: {
-      tsearch: { prefix: true }
+  # pg_search_scope :search_by_name_and_category,
+  #   against: [ :name, :category ],
+  #   using: {
+  #     tsearch: { prefix: true }
+  #   }
+
+  pg_search_scope :search,
+  against: [ :name, :category, :description, :accessories ],
+  using: {
+    tsearch: {
+      highlight: {
+        prefix: true,
+        StartSel: '<b>',
+        StopSel: '</b>',
+        MaxWords: 123,
+        MinWords: 456,
+        ShortWord: 4,
+        HighlightAll: true,
+        MaxFragments: 3,
+        FragmentDelimiter: '&hellip;'
+      }
     }
+  }
 
   ACCESSORIES = ["casque", "slip", "chaussures", "noeud papillon"]
   AVAILABILITY = ["✅ Disponible", "❌ Pas disponible"]
@@ -24,20 +42,20 @@ class Mouette < ApplicationRecord
 
   validates :availability, inclusion: {
       in: AVAILABILITY,
-      message: "Must select : #{AVAILABILITY}"
+      message: "Doit choisir : #{AVAILABILITY}"
   }
 
   validates :accessories, inclusion: {
       in: ACCESSORIES,
-      message: "Must select : #{ACCESSORIES}"
+      message: "Doit choisir : #{ACCESSORIES}"
   }
 
   validates :category, inclusion: {
     in: CATEGORIES,
-    message: "Must select : #{CATEGORIES}"
+    message: "Doit choisir : #{CATEGORIES}"
   }
 
-  validates :price, numericality: { greater_than: 0, message: "must be a number greater than 0" }
+  validates :price, numericality: { greater_than: 0, message: "doit être un nombre supérieur à 0" }
 
   validates :description, length: { minimum: 10 }
   # validates :rating, presence: true, numericality: { greater_than_or_equal_to: 0,less_than_or_equal_to: 5 }
